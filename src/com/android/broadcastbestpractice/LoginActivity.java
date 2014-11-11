545217f4a9399cf5689f597510344463bf1b706b
evitazhang <evitazhang@walltechsystem.cn>
2014-11-11 22:38:20 +0800

    添加数据库
    Change-Id: Idf9041ee12ba9d9e4482dc794d2a4b05e841f6a6

--- src/com/android/broadcastbestpractice/LoginActivity.java
+++ src/com/android/broadcastbestpractice/LoginActivity.java
@@ -1,14 +1,21 @@
 package com.android.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity {
+
+	private SharedPreferences pref;
+
+	private SharedPreferences.Editor editor;
 
 	private EditText userEdit;
 
@@ -16,20 +23,42 @@ public class LoginActivity extends BaseActivity {
 
 	private Button login;
 
+	private CheckBox checkBox;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_layout);
+		pref = PreferenceManager.getDefaultSharedPreferences(this);
 		userEdit = (EditText) findViewById(R.id.username);
 		pwdEdit = (EditText) findViewById(R.id.password);
 		login = (Button) findViewById(R.id.login);
+		checkBox = (CheckBox) findViewById(R.id.remember_pass);
+		boolean isRemember = pref.getBoolean("remember_pwd", false);
+		if (isRemember) {
+			String userName = pref.getString("userName", "");
+			String pwd = pref.getString("pwd", "");
+			userEdit.setText(userName);
+			pwdEdit.setText(pwd);
+			checkBox.setChecked(true);
+		}
 		login.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				String userName = userEdit.getText().toString();
 				String pwd = pwdEdit.getText().toString();
 				if (userName.equals("admin") && pwd.equals("123456")) {
+					editor = pref.edit();
+					if (checkBox.isChecked()) {
+						editor.putBoolean("remember_pwd", true);
+						editor.putString("userName", userName);
+						editor.putString("pwd", pwd);
+
+					} else {
+						editor.clear();
+					}
+					editor.commit();
 					Intent intent = new Intent(LoginActivity.this,
 							MainActivity.class);
 					startActivity(intent);
